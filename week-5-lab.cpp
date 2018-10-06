3aaf2f27a835ff679ec425e4dae1a041e4f024e6
Seonghyun Choi <43017312+kg41csh@users.noreply.github.com>
2018-10-06 00:48:55 -0400

    solved

---
+++ week-5-lab.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <math.h> // pow
+#include <fstream> // output file stream
+
+using namespace std;
+
+int main () {
+
+int first, second;
+
+cout << "Enter two integer : ";
+cin >> first >> second; 
+
+int width = pow(first, 2);
+int height = pow(second, 2);
+auto z = width + height;
+float result = sqrt(z);
+
+cout << "The Square root of (" << first << 
+"^2 + " << second << "^2)" << " = " << result << endl;
+
+ofstream makefile;
+makefile.open ("week-5-lab.txt");
+makefile << "Your triangle has 3 sides" << 
+" that are " <<  first  << "cm, " <<  second  << 
+"cm, and " << result << "cm";
+makefile.close(); 
+
+return 0;
+}
